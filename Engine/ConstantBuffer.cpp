f3ff8161d579ef9891920a44d5728de12a202fd1
hvvan <eminhwan12@gmail.com>
2022-11-01 17:31:08 +0900

    Material

--- Engine/ConstantBuffer.cpp
+++ Engine/ConstantBuffer.cpp
@@ -17,18 +17,19 @@ ConstantBuffer::~ConstantBuffer()
 	}
 }
 
-
-
-void ConstantBuffer::Init(uint32 size, uint32 count)
+void ConstantBuffer::Init(CBV_REGISTER reg, uint32 size, uint32 count)
 {
 	// 상수 버퍼는 256 바이트 배수로 만들어야 한다
 	// 0 256 512 768
+	_reg = reg;
 	_elementSize = (size + 255) & ~255;
 	_elementCount = count;
 
 	CreateBuffer();
 	CreateView();
 }
+
+
 
 void ConstantBuffer::CreateBuffer()
 {
@@ -77,17 +78,18 @@ void ConstantBuffer::Clear()
 	_currentIndex = 0;
 }
 
-D3D12_CPU_DESCRIPTOR_HANDLE ConstantBuffer::PushData(int32 rootParamIndex, void* buffer, uint32 size)
+void ConstantBuffer::PushData(void* buffer, uint32 size)
 {
-	assert(_currentIndex < _elementSize);
+	assert(_currentIndex < _elementCount);
+	assert(_elementSize == ((size + 255) & ~255));
 
 	::memcpy(&_mappedBuffer[_currentIndex * _elementSize], buffer, size);
 
 	D3D12_CPU_DESCRIPTOR_HANDLE cpuHandle = GetCpuHandle(_currentIndex);
 	
-	_currentIndex++;
+	GEngine->GetTableDescHeap()->SetCBV(cpuHandle, _reg);
 
-	return cpuHandle;
+	_currentIndex++;
 }
 
 D3D12_GPU_VIRTUAL_ADDRESS ConstantBuffer::GetGpuVirtualAddress(uint32 index)
